4d645fd77ccd757403653680d03e0a60cf9bbeb4
RafaelBarrero <ramag874@gmail.com>
2022-02-10 00:19:04 +0100

    feat(main.py): add chromedriver auto-download and fix deprecated functions
    * Delete chromedriver because chromedriver is now auto-downloaded

--- src/main.py
+++ src/main.py
@@ -1,22 +1,27 @@
+# Python imports
 import time
 
+# Application inner imports
 from config.settings import *
 
+# Selenium imports
+from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 def main():
-
+    service = Service(executable_path=ChromeDriverManager().install())
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--incognito')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
-    driver = webdriver.Chrome('chromedriver', options=chrome_options)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
     driver.delete_all_cookies()
 
     i = 0
@@ -28,7 +33,7 @@ def main():
 
         driver.get("https://id.heroku.com/login")
 
-        # Click cookies. No always
+        # Click cookies. Not always
         try:
             WebDriverWait(driver, 5)\
                 .until(ec.presence_of_element_located((By.ID, 'onetrust-accept-btn-handler'))).click()
@@ -39,10 +44,10 @@ def main():
         WebDriverWait(driver, 5).until(ec.presence_of_element_located((By.ID, 'email'))).send_keys(email)
 
         # Password
-        driver.find_element_by_id("password").send_keys(PASSWORD)
+        driver.find_element(By.ID, "password").send_keys(PASSWORD)
 
         # Log in
-        driver.find_element_by_name("commit").click()
+        driver.find_element(By.NAME, "commit").click()
 
         # No 2Factor. Not always
         try:
@@ -59,16 +64,16 @@ def main():
 
         # Click edit to dyno
         WebDriverWait(driver, 5).until(ec.presence_of_element_located((By.CLASS_NAME, 'clip')))
-        buttons = driver.find_elements_by_tag_name("button")
+        buttons = driver.find_elements(By.TAG_NAME, "button")
         for button in buttons:
             if "dyno" in button.text:
                 button.click()
 
         # Click toggle dyno
-        driver.find_element_by_class_name("cmn-toggle").click()
+        driver.find_element(By.CLASS_NAME, "cmn-toggle").click()
 
         # Click confirm
-        buttons = driver.find_elements_by_tag_name("button")
+        buttons = driver.find_elements(By.TAG_NAME, "button")
         for button in buttons:
             if "Confirm" in button.text:
                 button.click()
